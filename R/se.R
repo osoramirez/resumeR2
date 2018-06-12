992ebf480a051665f6983ce7d8424630172a6937
osoramirez <oscar.ramirez.alan@una.ac.cr>
2018-06-11 22:40:27 -0600

    New function The standard error

---
+++ R/se.R
@@ -0,0 +1,22 @@
+#'@title A standard error
+#'
+#'@description The standard error (SE) of a statistic is the standard deviation of its sampling distribution.
+#'@param x is a numeric value, could be a  a vector or data.frame
+#'@export
+#'@keywords
+#'@seealso \code{\link[utils]resume2data}
+#'@return a standard error
+#'@export
+#'
+#'@examples
+#'x<-rnorm(25,2,3)
+#'se(x)
+#
+se<-function (x,na.rm = TRUE)
+{
+  options(digits=3)
+  n <- length(x)
+  Stdev <- round(sd(x, na.rm = TRUE), 3)
+  SE <- round(Stdev/sqrt(n), 3)
+  return(SE)
+}
